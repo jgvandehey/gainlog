724d96894119e40f9aef59f1e81e3eef2a6088d4
Jeremy Vandehey <jgvandehey@gmail.com>
2015-01-09 22:17:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gainlog::Application.config.secret_token = 'bf9bdae41212403742f25c503c55fe20fc594418c7e6f09e6030a56dbf2800a3c369eb1b1b909a32571b2f7d00f89a3bcb2fc83b63d8620d7a67ebb6ff5eb6ad'
